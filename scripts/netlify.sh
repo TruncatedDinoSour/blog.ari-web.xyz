d82dae37a47b4672216df3abde5be78702d53aa2
Ari Archer <ari.web.xyz@gmail.com>
2023-10-06 20:56:22 +0300

    update @ Fri Oct  6 20:56:22 EEST 2023
    Signed-off-by: Ari Archer <ari.web.xyz@gmail.com>

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env sh
+!/usr/bin/env sh
 
 set -xeu
 
 main() {
     rustup toolchain install stable
     pip install -r requirements/requirements.txt
-    CI=1 NOCLR=1 python3 ./scripts/blog.py static
+    CI=1 NOCLR=1 time python3 ./scripts/blog.py static
     rm -rf ./scripts/ ./requirements/
 }
 
